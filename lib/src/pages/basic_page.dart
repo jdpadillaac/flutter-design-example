9fea275a92357b02cf09ca8881b5403669c31c5e
jonathan padilla <jonathan962911@gmail.com>
2020-10-04 00:53:14 -0500

    :heavy_check_mark: Feature: Basic design body with icons

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -6,13 +6,19 @@ class BasicPage extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        Image(
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067__340.png'),
-        ),
-        _rowWithTexts()
+        _headerImage(),
+        _rowWithTexts(),
+        _rowButtons(),
+        _bodyText()
       ],
     ));
+  }
+
+  Widget _headerImage() {
+    return Image(
+      image: NetworkImage(
+          'https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067__340.png'),
+    );
   }
 
   Widget _rowWithTexts() {
@@ -45,6 +51,44 @@ class BasicPage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget _rowButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _rowButtonsActions(Icons.call, 'Call'),
+        _rowButtonsActions(Icons.near_me, 'Route'),
+        _rowButtonsActions(Icons.share, 'Share')
+      ],
+    );
+  }
+
+  Widget _rowButtonsActions(IconData icon, String text) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 35.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          text,
+          style: TextStyle(fontSize: 12.0, color: Colors.blue),
+        )
+      ],
+    );
+  }
+
+  Widget _bodyText() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap"),
     );
   }
 }
